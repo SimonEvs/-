aa698f406aed5c93e7ff80074a45ba7eee0eeb4d
SimonEvs <simonevseev07@mail.ru>
2022-06-28 22:23:56 +0300

    add task 1

---
+++ seminars/2/task1/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число - {number} ");
+int digitOne =number /10;
+int digitTwo =number % 10;
+int result = Math.Max(digitOne,digitTwo);
+Console.WriteLine($"Наибольшая цифра - {result}");
+
+
