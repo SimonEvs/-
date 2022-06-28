ea697faf27869f166e0fa2327ec7719b7b73e889
SimonEvs <simonevseev07@mail.ru>
2022-06-28 22:35:00 +0300

    add task 2

---
+++ seminars/2/task2/Program.cs
@@ -0,0 +1,7 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число {number}");
+int digitOne= number/100;
+int digitThree= number % 10;
+Console.WriteLine("Склеиваем первую цифру и третью");
+
+Console.WriteLine($"Получаем: {digitOne}{digitThree}");
