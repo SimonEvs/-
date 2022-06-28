73b17cde510afa238054908a9bc3b6bc2c4d6828
SimonEvs <simonevseev07@mail.ru>
2022-06-28 23:15:49 +0300

    add task 3

---
+++ seminars/2/task3/Program.cs
@@ -0,0 +1,15 @@
+﻿ Console.WriteLine("Введите первое число ");
+int first = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+int second = Convert.ToInt32(Console.ReadLine());
+
+
+if (first % second == 0)
+{
+    Console.WriteLine("Числа делятся без остатка");
+}
+else
+{
+    Console.WriteLine($"Остаток: {first % second}");
+}
