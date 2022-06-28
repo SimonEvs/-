ad0cd0c0e2becd0e9a385baac4665b8a6aaa96cb
SimonEvs <simonevseev07@mail.ru>
2022-06-28 22:44:37 +0300

    add task 2

--- seminars/2/task2/Program.cs
+++ seminars/2/task2/Program.cs
@@ -1,7 +1,19 @@
-﻿int number = new Random().Next(100, 1000);
-Console.WriteLine($"Случайное число {number}");
-int digitOne= number/100;
-int digitThree= number % 10;
-Console.WriteLine("Склеиваем первую цифру и третью");
+﻿// int number = new Random().Next(100, 1000);
+// Console.WriteLine($"Случайное число {number}");
+// int digitOne= number/100;
+// int digitThree= number % 10;
+// Console.WriteLine("Склеиваем первую цифру и третью");
 
-Console.WriteLine($"Получаем: {digitOne}{digitThree}");
+// Console.WriteLine($"Получаем: {digitOne}{digitThree}");
+
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число {number}");
+
+int Result(int number)
+{
+    int first = number/100;
+    int third = number%10;
+    return first*10+third;
+}
+Console.WriteLine($"Склеиваем {Result(number)}");
