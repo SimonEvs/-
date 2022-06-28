406a5c1261cc58648239800666b19b43e8363f00
SimonEvs <simonevseev07@mail.ru>
2022-06-28 23:35:22 +0300

    add task 4

---
+++ seminars/2/task4/Program.cs
@@ -0,0 +1,12 @@
+﻿int wow=new Random().Next(161, 10000);
+Console.WriteLine(wow);
+bool Kratnost(int number)
+
+{
+    return (number % 7 == 0 && number % 23 == 0);
+
+
+}
+
+Console.WriteLine(Kratnost(wow));
+
