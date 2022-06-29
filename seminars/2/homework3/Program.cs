c37453cd2fbb08da9ae0fb292c60d3c0c3cd370e
SimonEvs <simonevseev07@mail.ru>
2022-06-29 20:16:01 +0300

    add HW3

---
+++ seminars/2/homework3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int wow = Convert.ToInt32(Console.ReadLine());
+int Proverka(int problem)
+{
+    if (problem ==6)
+    {
+        return(Console.WriteLine("Выходной "));
+    }
+}
+Console.WriteLine(Proverka(wow));
