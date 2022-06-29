7d0fdfd8f03dcbb37e3c4bc6de2818cca2c50d7b
SimonEvs <simonevseev07@mail.ru>
2022-06-29 19:45:41 +0300

    add HW2

---
+++ seminars/2/homework2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число ");
+int kek = Convert.ToInt32(Console.ReadLine());
+
+if (kek / 100 != 0)
+{
+    kek =kek/100;
+    kek = kek%10;
+    Console.Clear();
+    Console.WriteLine($"Третья цифра {kek}");
+    
+}
+else
+Console.WriteLine("Третьей цифры нету");
+// int Vivod(int kek)
+// {
+//     return( kek/100 ==555);
+//    // return (pool / 100 != 0 && pool % 1000> 99);
+// }
+
