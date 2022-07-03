493d671b5a513d64e0652b3a9b98fe325b0efd7b
SimonEvs <simonevseev07@mail.ru>
2022-07-03 10:58:29 +0300

    add task1

---
+++ seminars/3/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число от 1 до 4");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Значения переменных будут: ");
+if (number==1)
+{
+    Console.WriteLine("X>0;Y>0");
+}
+
+if (number==2)
+{
+    Console.WriteLine("X>0;Y<0");
+}
+
+if (number==3)
+{
+    Console.WriteLine("X<0;Y<0");
+}
+
+if (number==4)
+{
+    Console.WriteLine("X<0;Y>0");
+}
