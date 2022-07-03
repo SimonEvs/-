cf485873bf9e6cffa9a3fdc9e6e97633a3ab6f10
SimonEvs <simonevseev07@mail.ru>
2022-07-03 13:18:33 +0300

    add task2

---
+++ seminars/3/task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Впишите координаты первой точки: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Впишите координаты второй точки: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2));
+Console.WriteLine(result);
