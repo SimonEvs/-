36f0bf976c1e5d7c6bf3bc5d9b54c0fb70405a2c
SimonEvs <simonevseev07@mail.ru>
2022-07-03 13:54:34 +0300

    add task3

---
+++ seminars/3/task3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введитe число ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int second = 1;
+int result = 0;
+while (second <= N)
+{
+    result = second*second;
+    
+    Console.WriteLine($"Квадрат числа {second} = {result}" );
+second++;
+}
+
+// for (int i =1;i<=N; i++)
+// {
+//     Console.WriteLine($"Квадрат числа {i}={i*i}");
+// }
