18f32b269399567471dd017ccc8a46d1b84d0d77
SimonEvs <simonevseev07@mail.ru>
2022-07-03 23:38:55 +0300

    add for

--- other/urok3/for/Program.cs
+++ other/urok3/for/Program.cs
@@ -9,3 +9,14 @@
 }
 string res =Method(10,"z");
 Console.WriteLine(res);
+
+
+
+for(int i=2;i<=10;i++)
+{
+    for(int j=0;j<10;j++)
+    {
+    Console.WriteLine($"{i} x {j} ={i*j}");
+    }
+    Console.WriteLine();
+}
