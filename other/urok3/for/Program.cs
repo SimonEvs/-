b64af3b2107ab4c229a8a1928b912e66582f27a0
SimonEvs <simonevseev07@mail.ru>
2022-07-03 19:35:41 +0300

    add for

---
+++ other/urok3/for/Program.cs
@@ -0,0 +1,11 @@
+﻿string Method(int count, string text)
+{
+    string result =String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res =Method(10,"z");
+Console.WriteLine(res);
