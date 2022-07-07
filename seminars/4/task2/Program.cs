33c01b40ceef56ae47fe831fb31883220e37d0d3
SimonEvs <simonevseev07@mail.ru>
2022-07-07 21:24:05 +0300

    add task2

---
+++ seminars/4/task2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Принимаем на вход число и выводим количество цифр в числе.
+
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+while (number != 0) 
+{
+    number /= 10;
+    count++;
+}
+Console.WriteLine(count);
