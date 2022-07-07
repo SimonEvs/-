17340653551f6d354856cae39a34682868ecf308
SimonEvs <simonevseev07@mail.ru>
2022-07-07 22:04:14 +0300

    add task3

---
+++ seminars/4/task3/Program.cs
@@ -0,0 +1,11 @@
+﻿//факториал
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int result =1;
+
+for(int i = 2;i<=N;i++)
+{
+    result=N*result;
+}
+
+        Console.WriteLine($"Факториал числа = {result}");
