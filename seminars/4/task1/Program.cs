00617073303699a0dd4ee75e9296e2d644cd3285
SimonEvs <simonevseev07@mail.ru>
2022-07-07 21:01:55 +0300

    add task1

--- seminars/4/task1/Program.cs
+++ seminars/4/task1/Program.cs
@@ -1,8 +1,14 @@
 ﻿Console.WriteLine("Введите число ");
 int A = Convert.ToInt32(Console.ReadLine());
-int sum =0;
-for(int i =1;i<=A;i++)
+ int sum =0;
+// for(int i =1;i<=A;i++)
+// {
+//     sum+=i;
+// }
+ int i =1;
+while(i<=A)
 {
     sum+=i;
+    i++;
 }
 Console.WriteLine(sum);
