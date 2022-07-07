6551dbc289041b933b0a70ebe798a4333e2d5dfd
SimonEvs <simonevseev07@mail.ru>
2022-07-07 22:17:21 +0300

    add task4

---
+++ seminars/4/task4/Program.cs
@@ -0,0 +1,12 @@
+﻿//Массив из 8 элементов, заполненный нулями и единицами
+int[] array = BinaryArray(8);
+Console.WriteLine($"{String.Join("; ", array)}");
+int[] BinaryArray(int size)
+{
+    int[] resultArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        resultArray[i] = new Random().Next(0, 2);
+    }
+    return resultArray;
+}
