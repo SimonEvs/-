8955aa5393f90812b1ae1921480fd12664985404
SimonEvs <simonevseev07@mail.ru>
2022-07-08 20:15:12 +0300

    add HW3

---
+++ seminars/4/homework3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Массив из 8 чисел (сделал до сотни)
+
+int[]array = FillArray(8);
+Console.WriteLine($"[{String.Join(";", array)}]");
+
+int[] FillArray(int size)
+{
+    int[] array = new int [size];
+    for(int i=0;i<size;i++)
+    {
+        array[i] =new Random().Next(0,100);
+    }
+    return array;
+}
