21e40ef41f73f72836d4abf9697870eebc0c83c8
SimonEvs <simonevseev07@mail.ru>
2022-07-08 17:29:27 +0300

    add HW1

---
+++ seminars/4/homework1/Program.cs
@@ -0,0 +1,10 @@
+﻿//цикл, который принимает на вход два числа и возводит первое число в степень второго
+Console.Clear();
+Console.WriteLine("Введите переменную А ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine("Введите переменную В ");
+int B = Convert.ToInt32(Console.ReadLine());
+int C =Convert.ToInt32(Math.Pow(A,B));
+Console.Clear();
+Console.WriteLine($"{A} в степени {B} = {C}");
