b62dfd819a5c6e27e87ccfc9bb4a86a1031dffef
SimonEvs <simonevseev07@mail.ru>
2022-07-08 20:02:16 +0300

    add HW2

---
+++ seminars/4/homework2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Принимает на вход число, выводит сумму цифр в числе
+
+int number = Convert.ToInt32(Console.ReadLine());
+int number1=Convert.ToInt32(number);
+int count = 0;
+int C = 0;
+while (number != 0)
+{
+    number = number / 10;
+    count++;
+}
+Console.WriteLine($"Цифр в числе - {count}");
+
+for (int i = 0; i < count; i++)
+{
+    C = C + number1[i];
+}
+Console.WriteLine(C);
