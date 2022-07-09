fce50bb0a4338064497a4cd7701249eaaf2f3f11
SimonEvs <simonevseev07@mail.ru>
2022-07-09 12:16:21 +0300

    add task2

---
+++ seminars/5/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+int [] array = new int[12];
+int size=array.Length;
+
+int index=0;
+
+while (index<size)
+{
+    array[index] =new Random().Next(-9,10); 
+    index++;
+}
+Console.WriteLine($"[{String.Join(";", array)}]");
+
+for (int startIndex=0; startIndex<size;startIndex++)
+{
+    array[startIndex]*=-1;
+    
+}
+Console.WriteLine($"Поменяли значения [{String.Join(";",array)}]");
