1dc1f1f75dfa91a272655aceb2f119a6726a1812
SimonEvs <simonevseev07@mail.ru>
2022-07-10 15:01:10 +0300

    add HW3

---
+++ seminars/5/homework3/Program.cs
@@ -0,0 +1,27 @@
+﻿double[] array = new double[10];
+int size = array.Length;
+int index = 0;
+
+while (index != size)
+{
+    array[index] = new Random().Next(-100, 100);
+
+    index++;
+}
+double Min = array[0];
+double Max = array[0];
+for (index = 0; index != size; index++)
+{
+    if(Min>array[index])
+    {
+        Min=array[index];
+    }
+    
+    if (Max<array[index])
+    {
+        Max=array[index];
+    }
+}
+Console.WriteLine($"[{String.Join("; ", array)}]");
+Console.WriteLine($"Максимальное ={Max}; Минимальное ={Min}");
+Console.WriteLine($"Разность = {Max-Min}");
