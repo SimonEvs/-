48759c0faafacb18f0cc5a0d5fea84707e5ef01a
SimonEvs <simonevseev07@mail.ru>
2022-07-10 13:49:37 +0300

    add HW2

---
+++ seminars/5/homework2/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = new int[6];
+int size = array.Length;
+int index = 0;
+int sum = 0;
+double currentNumber = 0;
+
+while (index < size)
+{
+    array[index] = new Random().Next(1, 1000);
+    currentNumber = array[index];
+    if ((currentNumber % 2)> 0)
+    {
+        sum+=array[index];
+    }
+    index++;
+}
+Console.WriteLine($"[{String.Join("; ", array)}];");
+Console.WriteLine($"Сумма нечетных = {sum}.");
