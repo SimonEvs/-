67082764218cf21b3d5ec15425fa4c41447cd4de
SimonEvs <simonevseev07@mail.ru>
2022-07-12 19:12:07 +0300

    Двумерные массивы

--- other/urok4/massiv1/Program.cs
+++ other/urok4/massiv1/Program.cs
@@ -21,8 +21,9 @@
 //     Console.WriteLine();
 // }
 
-void PrintArray(int [,] matr);
+void PrintArray(int[,] matr)
 {
+
     for (int i = 0; i < matr.GetLength(0); i++)
     {
         for (int j = 0; j < matr.GetLength(1); j++)
@@ -32,7 +33,22 @@ void PrintArray(int [,] matr);
         Console.WriteLine();
     }
 }
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+
+    }
+}
+
 int[,] matrix = new int[3, 4];
 
 
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
 PrintArray(matrix);
