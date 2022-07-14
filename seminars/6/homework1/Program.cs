9706fb5a9a41dd412353f7900c729af622a5b1cf
SimonEvs <simonevseev07@mail.ru>
2022-07-14 23:00:39 +0300

    HW1 massiv&>0

---
+++ seminars/6/homework1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine($"Введите размер ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] newArray = new int[size];
+Console.Clear();
+int count =0;
+for (int i = 0; i != size; i++)
+{
+
+
+    Console.WriteLine($"Введите{i + 1} число ");
+    newArray[i] = Convert.ToInt32(Console.ReadLine());
+    Console.Clear();
+    if(newArray[i]>0)
+    {
+        count++;
+    }
+
+}
+
+Console.WriteLine($"[{String.Join("; ", newArray)}]");
+Console.WriteLine($"Чисел больше 0 :{count}");
