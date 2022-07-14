2a4ecd7b7a8db61d316f2feeeb123cd22a5ffd55
SimonEvs <simonevseev07@mail.ru>
2022-07-14 22:23:02 +0300

    task4

---
+++ seminars/6/task4/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+Console.WriteLine("Введите число 10системы исчисления ");
+int inputNumber= Convert.ToInt32(Console.ReadLine());
+
+void Binary(int number)
+{
+    string result = "";
+    while (number > 0)
+    {
+        result = number % 2 + result;
+        number = number / 2;
+    
+    }
+    Console.WriteLine($"В двоичной будет так={result}");
+}
+Binary(inputNumber);
