386242e9bfbc6b5378cce7e58263b0f6152f7a22
SimonEvs <simonevseev07@mail.ru>
2022-07-14 21:41:46 +0300

    task2

---
+++ seminars/6/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которач принимает на вход три числа и проверяет
+// може ли существовать труегольник с сторонами такой длины.
+
+int firstSide=3;
+int secondSide=7;
+int thirdSide=71;
+
+if(firstSide+secondSide>thirdSide&&secondSide+thirdSide>firstSide&&firstSide+thirdSide>secondSide)
+{
+    Console.WriteLine("Существует");
+}
+else
+Console.WriteLine("Не существует");
