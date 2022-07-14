b9ef17ea26b90a9acd819ded2a840150afd00058
SimonEvs <simonevseev07@mail.ru>
2022-07-14 22:06:40 +0300

    task3

---
+++ seminars/6/task3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Не используя рекурсию, выведите первые N чисел Фибоначчи.
+//Первые два числа это 0 и 1.
+int N=10;
+int firstElement=0;
+int secondElement=1;
+
+Console.WriteLine(firstElement);
+Console.WriteLine(secondElement);
+
+
+for(int index=3;index<=N;index++)
+{
+    int nextElement =firstElement+secondElement;
+    Console.WriteLine(nextElement);
+    firstElement=secondElement;
+    secondElement=nextElement;
+
+}
