ad8596b1188a580b6616c4b0fbfbe0dfdffac8e2
SimonEvs <simonevseev07@mail.ru>
2022-07-16 17:43:09 +0300

    HW2 sdelan

--- seminars/6/homework2/Program.cs
+++ seminars/6/homework2/Program.cs
@@ -1,14 +1,14 @@
 ﻿Console.WriteLine("Вводим значиене k1");
-int k1=Convert.ToInt32(Console.ReadLine());
+double k1=Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Вводим значиене k2");
-int k2=Convert.ToInt32(Console.ReadLine());
+double k2=Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Вводим значиене b1");
-int b1=Convert.ToInt32(Console.ReadLine());
+double b1=Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Вводим значиене b2");
-int b2=Convert.ToInt32(Console.ReadLine());
-double x=((b2-b1)/(k1-k2))+((b2-b1)%(k1-k2));
+double b2=Convert.ToInt32(Console.ReadLine());
+double x=(b2-b1)/(k1-k2);
 double y=(k1*x+b1);
-Console.WriteLine($"x={x},y={y}");
+Console.WriteLine($"x={x},y= {y}");
 
 
 
