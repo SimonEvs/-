ded4a8e7ec6b433f5abb46691a5faefc0360aec5
SimonEvs <simonevseev07@mail.ru>
2022-07-17 15:52:42 +0300

    matrix

---
+++ seminars/7/task1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задайте двумерный массив размером m*n, заполненный случайными целыми числами.
+
+int rows = Int32.Parse(Console.ReadLine());
+int columns = Int32.Parse(Console.ReadLine());
+double[,] matrix = new double[rows, columns];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(1,100);
+        //NextDouble() 0,544444*100=54.NextDouble-от 0 до 1.
+        Console.Write(matrix[i, j] + "\t");
+        //T- Tab
+        //n-new
+        // это все литералы
+
+    }
+    Console.WriteLine();
+}
