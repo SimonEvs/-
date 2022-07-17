496ec0e269d38246cb81ba1c2fe6b2a2e546202c
SimonEvs <simonevseev07@mail.ru>
2022-07-17 17:07:23 +0300

    matrix task4

--- seminars/7/task4/Program.cs
+++ seminars/7/task4/Program.cs
@@ -2,16 +2,17 @@
 //замениет элементы на их квадраты *по главной диагонали*
 
 int[,] matrix={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
-
+int sum=0;
 for (int i = 0; i < matrix.GetLength(0); i++)
 {
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
         if (j==i)
         {
-            matrix[i, j] *=matrix[i, j];
+            sum+=matrix[i, j];
         }
         Console.Write(matrix[i, j]+"\t");
     }
     Console.WriteLine();
 }
+Console.WriteLine(sum);
