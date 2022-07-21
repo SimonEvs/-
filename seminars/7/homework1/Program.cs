78445186507308daf9d8f59d7f04da1c6f24bb0c
SimonEvs <simonevseev07@mail.ru>
2022-07-21 17:02:11 +0300

    HW1

--- seminars/7/homework1/Program.cs
+++ seminars/7/homework1/Program.cs
@@ -1,17 +1,21 @@
 ﻿//Задайте двумерный массив размерами m*n, заполненный случайными вещественными числами.
-
+Console.Clear();
+Console.WriteLine("Введите количество строк ");
 int rows = Int32.Parse(Console.ReadLine());
+Console.Clear();
+Console.WriteLine("Введите количество столбцов ");
 int columns = Int32.Parse(Console.ReadLine());
+Console.Clear();
 double[,] matrix = new double[rows, columns];
-for(int i=0;i<matrix.GetLength(0);i++)
+for (int i = 0; i < matrix.GetLength(0); i++)
 {
 
-    
+
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        matrix[i, j] =  new Random().NextDouble()*100;    
-        Console.Write(matrix[i, j] + "\t"); 
-
+        matrix[i, j] = new Random().NextDouble() * 100;
+        Console.Write(Math.Round(matrix[i, j],2) + "\t");
+        
     }
     Console.WriteLine();
 }
