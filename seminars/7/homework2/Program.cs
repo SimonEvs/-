89702b9c3a911070682c9e7d24b306620bb19f3c
SimonEvs <simonevseev07@mail.ru>
2022-07-21 17:32:02 +0300

    HW2

---
+++ seminars/7/homework2/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Clear();
+Console.WriteLine("Введите количество строк ");
+int rows = Int32.Parse(Console.ReadLine());
+Console.Clear();
+Console.WriteLine("Введите количество столбцов ");
+int columns = Int32.Parse(Console.ReadLine());
+Console.Clear();
+double[,] matrix = new double[rows, columns];
+int rownumber = matrix.GetLength(0);
+int columnnumber = matrix.GetLength(1);
+for (int i = 0; i < rownumber; i++)
+{
+
+
+    for (int j = 0; j < columnnumber; j++)
+    {
+        matrix[i, j] = new Random().Next(0, 100);
+        Console.Write(matrix[i, j] + "\t");
+
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Какое число хотите найти? ");
+int find = Convert.ToInt32(Console.ReadLine());
+int variable = 0;
+for (int i = 0; i < rownumber; i++)
+{
+
+
+    for (int j = 0; j < columnnumber; j++)
+    {
+        if (matrix[i, j] != find)
+        {
+        
+            variable += 1;
+        }
+
+
+    }
+  
+}
+if (variable == rownumber * columnnumber)
+{
+    Console.WriteLine("Нету такого числа");
+}
+else{Console.WriteLine("Есть такое число");}
