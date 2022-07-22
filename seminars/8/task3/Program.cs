ac47eebd7d4d42448d39e2d73ef2b68fbb29c0f7
SimonEvs <simonevseev07@mail.ru>
2022-07-22 23:35:53 +0300

    task3

---
+++ seminars/8/task3/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, 
+//на пересечении которых расположен наименьший элемент массива. 
+
+int rows = 3;
+
+int columns = 3;
+
+int[,] matrix = new int[rows, columns];
+int min = int.MaxValue;
+int indexRows = 0;
+int indexColumns = 0;
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        matrix[i, j] = new Random().Next(11);
+        Console.Write(matrix[i, j] + "\t");
+        if (min > matrix[i, j])
+        {
+            min = matrix[i, j];
+            indexRows = i;
+            indexColumns = j;
+
+        }
+    }
+    Console.WriteLine();
+}
+Console.WriteLine(min);
+
+for (int i = 0; i < rows; i++)
+{
+    if (indexRows != i)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (indexColumns != j)
+            {
+                Console.Write(matrix[i, j]+"\t");
+            }
+        }
+         Console.WriteLine();
+    }
+}
