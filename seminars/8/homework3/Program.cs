08545d642ccf368eccb6bf9f4aa18f8e931d3089
SimonEvs <simonevseev07@mail.ru>
2022-07-23 01:52:00 +0300

    HW3

---
+++ seminars/8/homework3/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Введите количество строк в массивах: ");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в массивах:");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+
+
+int[,] matrix1 = new int[rows, columns];
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        matrix1[i, j] = new Random().Next(0, 10);
+        Console.Write(matrix1[i, j] + "\t");
+
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+int[,] matrix2 = new int[rows, columns];
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        matrix2[i, j] = new Random().Next(0, 10);
+        Console.Write(matrix2[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+int[,] multiplyArray = new int[rows, columns];
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        multiplyArray[i, j] = matrix1[i, j] * matrix2[i, j];
+        Console.Write(multiplyArray[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
