6ef625aaa0bdd34f346459e112a86372f41043c0
SimonEvs <simonevseev07@mail.ru>
2022-07-24 18:26:51 +0300

    power

--- other/urok7/power/Program.cs
+++ other/urok7/power/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вычислить А в степени n.
+// 0 на 0 тоже 1.
+// Итеративный подход.
+
+int PowerFor(int a, int n)
+{
+    int result =1;
+    for (int i=1;i<=n;i++) result *=a;
+    return result;
+}
+
+// Рекурсивный подход.
+int PowerRec(int a, int n)
+{  //return n==0?:PowerRec(a,n-1)a;
+if (n==0)return 1;
+else return PowerRec(a,n-1)*a;
+}
+
+Console.WriteLine(PowerFor(2,10));
+Console.WriteLine(PowerRec(2,10));
