778a446bb420446edcf344fb69f4da855fab6b65
SimonEvs <simonevseev07@mail.ru>
2022-07-24 18:00:40 +0300

    sum from 1 to n

---
+++ other/urok7/sum1toN/Program.cs
@@ -0,0 +1,20 @@
+﻿//Сумма чисел от 1 до n.
+
+// Итеративный подход.
+int SumFor(int n)
+{
+    int result = 0;
+    for (int i = 1; i <= n; i++) result += i;
+    return result;
+}
+
+//Рекурсивный подход.
+int SumRec(int n)
+{
+    if (n == 0) return 0;
+    else return n + SumRec(n - 1);
+
+}
+
+Console.WriteLine(SumFor(5));
+Console.WriteLine(SumRec(5));
