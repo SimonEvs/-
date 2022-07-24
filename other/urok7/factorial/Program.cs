06e053603589c5cfa8748f4ef61bb6f69f9565e5
SimonEvs <simonevseev07@mail.ru>
2022-07-24 17:57:36 +0300

    factorial

---
+++ other/urok7/factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿//Факториал.
+
+//Итеративный подход.
+
+int FactorialFor(int n)
+{
+    int result=1;
+    for(int i=1;i<=n;i++) result *=i;
+    return result;
+}
+//Рукурсивный подход.
+int FactorialRec(int n)
+{
+    if(n==1) return 1;
+    else return n*FactorialRec(n-1);
+}
+
+Console.WriteLine(FactorialFor(5));
+Console.WriteLine(FactorialRec(5));
