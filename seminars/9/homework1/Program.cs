98e4f6463c090fd056b2cc52186d4ad7842e599f
SimonEvs <simonevseev07@mail.ru>
2022-07-26 10:09:57 +0300

    homework1

---
+++ seminars/9/homework1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задайте значения M и N.
+//Напишите программу, которая выведет все натуральные 
+//числа в промежутке от M до N.
+
+Console.WriteLine("Введите N ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите M ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(PrintNumbers(n, m));
+
+string PrintNumbers(int start, int end)
+{
+    if (start == end) return start.ToString();
+    return (start + "," + PrintNumbers(start + 1, end));
+}
