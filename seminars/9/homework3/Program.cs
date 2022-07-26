bf512b3c120abfeb06ce46b947a486b4e456b09e
SimonEvs <simonevseev07@mail.ru>
2022-07-26 10:40:57 +0300

    homework3 Akkerman

---
+++ seminars/9/homework3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Функция Аккермана с помощью рекурсии.
+
+
+
+Console.WriteLine("Введите Первое ");
+int first = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите Второе ");
+int second = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Функция Аккермана {Akker(first, second)}");
+
+int Akker(int first, int second)
+{
+    if (first == 0) return second + 1;
+    if (first > 0 && second == 0) return Akker(first - 1, 1);
+    if (first > 0 && second > 0) return Akker(first - 1, Akker(first, second - 1));
+}
