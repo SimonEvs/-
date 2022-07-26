3c5057cb3f68274378fc25f7baa7b4a086dcba06
SimonEvs <simonevseev07@mail.ru>
2022-07-26 09:46:09 +0300

    task recursion2

---
+++ seminars/9/task2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход будет принимать число,
+//  а на выход выводить сумму его цифр.
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int SumNumbers(int number)
+{
+    if (number==0) return 0;
+    return (number%10+SumNumbers(number/10));
+}
+
+Console.WriteLine($"Сумма = {SumNumbers(number)}");
