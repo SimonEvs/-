b711d6630c483f8b3a65f80059fc165f44f15188
SimonEvs <simonevseev07@mail.ru>
2022-07-26 10:23:39 +0300

    homework2

---
+++ seminars/9/homework2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая будет находить сумму натуральных чисел
+// от заданного промежутка от M до N
+
+
+Console.WriteLine("Введите N ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите M ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма промежутка равна = {SumNumber(n, m)}");
+
+int SumNumber(int first,int second)
+{
+    if (first==second) return first;
+    return first+SumNumber(first+1,second);
+}
