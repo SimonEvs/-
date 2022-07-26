27dc628b8b76f808c9d5fd0b2b72fa18a1b12711
SimonEvs <simonevseev07@mail.ru>
2022-07-26 09:31:45 +0300

    task recursion

---
+++ seminars/9/task1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задайте значение N
+// Напишите программу, которая выведет
+// все натуральные числа от 1 до N.
+//рекурсия
+
+Console.WriteLine("Введите N ");
+int n=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(PrintNumbers(1,n));
+
+string PrintNumbers(int start, int end)
+{
+    if (start==end) return start.ToString();
+    return(start+","+PrintNumbers(start+1,end));
+}
