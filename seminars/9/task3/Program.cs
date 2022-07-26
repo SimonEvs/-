693efe6399944dc9d9ce03f14366da652e964bc0
SimonEvs <simonevseev07@mail.ru>
2022-07-26 09:59:54 +0300

    task recursion3

---
+++ seminars/9/task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход будет принимать два числа А и В,
+//  и возводит число А в целую степень В с помощью рекурсии.
+
+Console.WriteLine("Введите число A ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B ");
+int second = Convert.ToInt32(Console.ReadLine());
+
+int PowerNumbers(int first,int second)
+{
+    if (second==0) return 1;
+    return (first*PowerNumbers(first,second-1));
+}
+
+Console.WriteLine($"Степень = {PowerNumbers(first,second)}");
